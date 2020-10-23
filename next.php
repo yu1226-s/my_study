e817f74e0a30ffd206a083bc12673496cfeabd37
yu1226 <playstationtaka02@gmail.com>
2020-10-23 11:15:19 +0900

    タイムゾーン設定し忘れてた

--- next.php
+++ next.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>お次はブランチの練習</title>
-</head>
-<body>
-<?php 
-echo htmlspecialchars(date('G:i'));
-?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+     <title>お次はブランチの練習</title>
+    </head>
+    <body>
+        <p>こんにちは</p>
+        <?php 
+        date_default_timezone_set('Asia/Tokyo');
+         echo date('G:i');
+        ?>
+    </body>
+</html>
